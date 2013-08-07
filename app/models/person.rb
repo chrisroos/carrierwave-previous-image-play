58a74b255cb9e356566063250911f9fa193dd18e
Chris Roos <chris@seagul.co.uk>
2013-08-07 14:00:07 +0100

    Add avatar uploader and mount on person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,3 @@
 class Person < ActiveRecord::Base
+  mount_uploader :avatar, AvatarUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130807125701) do
+ActiveRecord::Schema.define(version: 20130807125913) do
 
   create_table "people", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
 end
