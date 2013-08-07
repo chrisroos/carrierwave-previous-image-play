33f9f7f54bf96379630e0db8a4631a4eacd4ed36
Chris Roos <chris@seagul.co.uk>
2013-08-07 14:02:13 +0100

    Add simple test to ensure uploading works

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "uploading an avatar" do
+    person = Person.create!
+    person.avatar = Tempfile.new(['original-avatar', '.jpg'])
+    person.save!
+
+    assert_match /original-avatar/, person.avatar_url
+    assert_match /original-avatar/, person.avatar.url
+  end
 end
