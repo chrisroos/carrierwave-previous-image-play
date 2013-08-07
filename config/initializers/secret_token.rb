e9fd8fd41a46a2a3c501aa047368f1742da69877
Chris Roos <chris@seagul.co.uk>
2013-08-07 13:55:16 +0100

    Empty Rails 4.0 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarrierwavePreviousImagePlay::Application.config.secret_key_base = '50ad6318ec1842d5e53d24ff3ac3141b6f3e4989878e36ff1719d074806b13e9b4879dd299aa294878308c53bfa8f104476df9f5b76e466b006f5d904fbf8694'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CarrierwavePreviousImagePlay::Application.config.session_store :cookie_store, key: '_carrierwave-previous-image-play_session'
