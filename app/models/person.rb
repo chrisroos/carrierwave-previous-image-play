c219ee95060c82e2765c59d405a1318ee34d4854
Chris Roos <chris@seagul.co.uk>
2013-08-07 14:03:35 +0100

    Require people to have names

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,4 @@
 class Person < ActiveRecord::Base
+  validates :name, presence: true
   mount_uploader :avatar, AvatarUploader
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
+  test "without a name should be invalid" do
+    person = Person.new(name: nil)
+    refute person.valid?
+    assert person.errors[:name].any?
+  end
+
   test "uploading an avatar" do
-    person = Person.create!
+    person = Person.create!(name: 'Chris')
     person.avatar = Tempfile.new(['original-avatar', '.jpg'])
     person.save!
 
